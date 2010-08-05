d7d1fe05b6af00fd97d9fe5d238b077bd8a298c9
Stanisław Pitucha <viraptor@gmail.com>
2010-08-05 03:17:36 +0100

    added packaging / pypi stuff

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='arpy',
+		version='0.1.0',
+		description='Library for accessing "ar" files',
+		author='Stanisław Pitucha',
+		author_email='viraptor@gmail.com',
+		url='http://bitbucket.org/viraptor/arpy',
+		py_modules=['arpy'],
+		)
+
