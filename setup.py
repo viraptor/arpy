5b5551c64cdde6ccc36c28b6718aeb9a17e0daa3
Stanisław Pitucha <viraptor@gmail.com>
2011-02-08 22:56:55 +0000

    bump version to include license in pypi

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from distutils.core import setup
 
 setup(name='arpy',
-		version='0.1.0',
+		version='0.1.1',
 		description='Library for accessing "ar" files',
 		author=u'Stanisław Pitucha',
 		author_email='viraptor@gmail.com',
