739965d397d567f6f11ddc7944c159f20eadb9f9
Stanisław Pitucha <viraptor@gmail.com>
2013-06-30 22:17:39 +0100

    Version 1.1.0
    Added stream support, iteration of main archive, travis-ci integration.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup
 
 setup(name='arpy',
-		version='1.0.0',
+		version='1.1.0',
 		description='Library for accessing "ar" files',
 		author=u'Stanisław Pitucha',
 		author_email='viraptor@gmail.com',
