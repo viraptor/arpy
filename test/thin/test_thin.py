851f4964a0a77cedd95587186572a6f73e55f55e
Stanisław Pitucha <viraptor@gmail.com>
2021-01-13 21:11:40 +1100

    Make sonar more happy

--- test/thin/test_thin.py
+++ test/thin/test_thin.py
@@ -5,13 +5,14 @@ import os
 # Test thin archive support
 class Thin(unittest.TestCase):
     thin_file_name = b'CMakeFiles/ext_lib_normal.dir/ext_lib.c.o'
+    thin_ar_name = 'thin.ar'
 
     def get_file_content(self):
         with open(os.path.dirname(__file__)+'/CMakeFiles/ext_lib_normal.dir/ext_lib.c.o','rb') as f:
             return f.read()
 
     def test_list(self):
-        ar = arpy.Archive(os.path.join(os.path.dirname(__file__), 'thin.ar'))
+        ar = arpy.Archive(os.path.join(os.path.dirname(__file__), self.thin_ar_name))
         ar.read_all_headers()
         self.assertEqual([self.thin_file_name],
                 list(ar.archived_files.keys()))
@@ -19,7 +20,7 @@ class Thin(unittest.TestCase):
         ar.close()
 
     def test_content(self):
-        ar = arpy.Archive(os.path.join(os.path.dirname(__file__), 'thin.ar'))
+        ar = arpy.Archive(os.path.join(os.path.dirname(__file__), self.thin_ar_name))
         ar.read_all_headers()
         arpy_entry = ar.archived_files[self.thin_file_name]
         real_content = self.get_file_content()
@@ -29,7 +30,7 @@ class Thin(unittest.TestCase):
         ar.close()
 
     def test_content_offset_preserving(self):
-        ar = arpy.Archive(os.path.join(os.path.dirname(__file__), 'thin.ar'))
+        ar = arpy.Archive(os.path.join(os.path.dirname(__file__), self.thin_ar_name))
         ar.read_all_headers()
         arpy_entry = ar.archived_files[self.thin_file_name]
         real_content = self.get_file_content()
