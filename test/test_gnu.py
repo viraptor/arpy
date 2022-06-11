93d9a87b2fbeb50076c1d25a7b5984b8a512fc71
Stanisław Pitucha <viraptor@gmail.com>
2022-06-11 14:56:50 +1000

    Add a test for null-separated entries

--- test/test_gnu.py
+++ test/test_gnu.py
@@ -20,6 +20,15 @@ class GNUExtendedNames(unittest.TestCase):
 		self.assertEqual(3, len(ar.headers))
 		ar.close()
 	
+	def test_multi_name_with_null_separator(self):
+		ar = arpy.Archive(os.path.join(os.path.dirname(__file__), 'msvc_lib.ar'))
+		ar.read_all_headers()
+		self.assertEqual([b'a_very_long_name_for_the_gnu_type_header_so_it_can_overflow_the_standard_name_length',
+			b'a_very_long_name_for_the_gnu_type_header_so_it_can_overflow_the_standard_name_length_with_space '],
+			sorted(ar.archived_files.keys()))
+		self.assertEqual(3, len(ar.headers))
+		ar.close()
+
 	def test_mixed_names(self):
 		ar = arpy.Archive(os.path.join(os.path.dirname(__file__), 'gnu_mixed.ar'))
 		ar.read_all_headers()
